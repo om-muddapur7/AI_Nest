e78e02b5cf112f74aa522f16434b5b5f849f4719
Om Muddapur <ommuddapur7@gmail.com>
2026-07-02 18:58:31 +0530

    clerk auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,32 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import Layout from "./pages/Layout"
+import Dashboard from "./pages/Dashboard"
+import WriteArticle from "./pages/WriteArticle"
+import BlogTitles from "./pages/BlogTitles"
+import GenerateImages from "./pages/GenerateImages"
+import RemoveBackground from "./pages/RemoveBackground"
+import ReviewResume from "./pages/ReviewResume"
+import Community from "./pages/Community"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout /> }>
+          <Route index element={<Dashboard />} />
+            <Route path='write-articles' element={<WriteArticle />} />
+            <Route path='blog-titles' element={<BlogTitles />} />
+            <Route path='generate-images' element={<GenerateImages />} />
+            <Route path='remove-background' element={<RemoveBackground />} />
+            <Route path='review-resume' element={<ReviewResume />} />
+            <Route path='community' element={<Community />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/pages/WriteArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WriteArticel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WriteArticel
