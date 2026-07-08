cc6bd02b71b0cee39ceb9f7ad2da521a73730325
Om Muddapur <ommuddapur7@gmail.com>
2026-07-08 17:14:26 +0530

    user api

---
+++ server/controllers/userController.js
@@ -0,0 +1,80 @@
+import sql from "../configs/db.js";
+
+export const getUserCreations = async () => {
+	try {
+		const { userId } = req.auth();
+
+		const creations =
+			await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY created_at DESC`;
+
+		res.json({
+			success: true,
+			message: creations,
+		});
+	} catch (error) {
+		res.json({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+
+export const getPublishedCreations = async () => {
+	try {
+		const creations =
+			await sql`SELECT * FROM creations WHERE publish = true ORDER BY created_at DESC`;
+
+		res.json({
+			success: true,
+			message: creations,
+		});
+	} catch (error) {
+		res.json({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+
+export const toggleLikeCreations = async () => {
+	try {
+		const { userId } = req.auth();
+		const { id } = req.body;
+
+		const [creation] = await sql`SELECT * FROM creations WHERE id = ${id}`;
+
+		if (!creation) {
+			return res.json({
+				success: false,
+				message: "creation not found",
+			});
+		}
+
+        const currentLikes = creation.likes;
+        const userIdStr = userId.toSring();
+        let updateLikes;
+        let message;
+
+        if(currentLikes.includes(userIdStr)){
+            updateLikes = currentLikes.filter((user) => user !== userIdStr);
+            message = 'Creation unliked'
+        } else {
+            updateLikes = [...currentLikes, userIdStr];
+            message = 'Creation liked'
+        }
+
+        const formattedArray = `{${updateLikes.join(',')}}`
+
+        await sql`UPDATE creations SET likes = ${formattedArray}::text[] WHERE id = ${id}`;
+
+		res.json({
+			success: true,
+			message: message,
+		});
+	} catch (error) {
+		res.json({
+			success: false,
+			message: error.message,
+		});
+	}
+};
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import { clerkMiddleware} from "@clerk/express";
 import aiRouter from "./routes/aiRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,7 @@ await connectCloudinary();
 app.get("/", (req, res) => res.send("Server is Live!"));
 
 app.use("/api/ai", aiRouter);
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
